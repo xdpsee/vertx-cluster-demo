b60cf36fed478702cce7ed3fbb7136979eb432e7
zhenhui.czh <zhenhui.czh@alibaba-inc.com>
2019-04-28 11:09:54 +0800

    aduit finished

---
+++ webapp/src/components/home/Main.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="main">
+        <h1>{{msg}}</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Main',
+        data: () => {
+            return {
+                msg: 'heeheheheh'
+            }
+        }
+    }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+    .main {
+
+    }
+</style>
